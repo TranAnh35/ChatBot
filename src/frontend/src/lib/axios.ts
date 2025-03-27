f9162ecf3d9204f739bee736008d45f1e9b5cb57
VoDanhDay <tranleanh3524@gmail.com>
2025-03-27 21:26:35 +0700

    Tăng timeout để chương trình có thêm thời gian xử lý

--- src/frontend/src/lib/axios.ts
+++ src/frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:8000', // URL backend
-  timeout: 10000, // Thời gian chờ tối đa
+  timeout: 20000, // Thời gian chờ tối đa
 });
 
 export default api;
